7ec4e5f84c6deef2ec90b9021753cd57b767983f
Nathaniel Alba <nathaniel.alba@gmail.com>
2016-08-04 18:18:30 -0700

    Add webpack and start the app

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 4000;
+const path = require('path');
 const app = express();
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+require('./routes')(app);
 
 app.listen(PORT);
 console.log('Server is running on port:', PORT);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app/index.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/app.js'
+  },
+  resolve: {
+    root: __dirname,
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
